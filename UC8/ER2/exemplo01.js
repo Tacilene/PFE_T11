1d8a0aa8c7d012b061b071e7c0c0cadc84f94584
USER@DESKTOP <palhetacorreatacilene@gmail.com>
2022-09-23 15:14:08 -0300

    Resolução da atividade5-ER2

---
+++ UC8/ER2/exemplo01.js
@@ -0,0 +1,10 @@
+/*regras para operadores de comparação
+= (é utilizado para atribuição)
+== para comparação (verifica se é igual)
+
+*/
+
+let x = 20
+let y = '20'
+let resultado = (x==y)
+console.log(resultado)
